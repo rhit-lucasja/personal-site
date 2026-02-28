fa847dc1db588b9c71880c9efaaa7e972e4fc1ab
rhit-lucasja <lucasja@rose-hulman.edu>
2026-02-28 13:27:03 -0500

    Added navbar styling (and some commented out code for a background animation, needs tweaks)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,15 @@ import pfp from '../assets/red.jpg';
 const Home = () => {
     return (
         <div className="min-h-screen flex items-center justify-center bg-white">
-            <div className="text-center space-y-6">
+            {/* background
+            <div className="absolute top-0 left-0 w-full h-full">
+                <div className="animate-circle animation-delay-2000 absolute top-20 left-20 w-72 h-72 bg-purple-200 rounded-full mix-blend-multiply filter blur-xl opacity-70" />
+                <div className="animate-circle animation-delay-4000 absolute top-40 right-20 w-72 h-72 bg-blue-200 rounded-full mix-blend-multiply filter blur-xl opacity-70" />
+                <div className="animate-circle absolute bottom-20 left-1/2 w-72 h-72 bg-pink-200 rounded-full mix-blend-multiply filter blur-xl opacity-70" />
+            </div>*/}
+            
+            {/* tagline & links */}
+            <div className="relative text-center space-y-6">
                 <img src={pfp} alt="Profile Photo" className="w-40 h-50 rounded-4xl object-cover mx-auto" />
                 <h1 className="text-5xl font-bold text-gray-900">
                     Jack Lucas

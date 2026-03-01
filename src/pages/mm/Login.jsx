00a3c3984db215ee98229b87d1febb21c4af1320
rhit-lucasja <lucasja@rose-hulman.edu>
2026-03-01 17:43:05 -0500

    created function and states for Firebase auth, no login page UI yet

--- src/pages/mm/Login.jsx
+++ src/pages/mm/Login.jsx
@@ -1,7 +1,40 @@
-
+import { useState } from 'react';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../firebase';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../context/AuthContext';
 
 const Login = () => {
-    return <h1>Login</h1>;
+    // get login info as states
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState(null);
+    const navigate = useNavigate();
+    const { user } = useAuth();
+
+    // if logged in, redirect to admin dashboard
+    if (user) {
+        navigate('/mm/admin');
+    }
+
+    // blocking function to attempt Firebase sign-in
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        setError(null);
+        try {
+            // sign in and then redirect to admin page
+            await signInWithEmailAndPassword(auth, email, password);
+            navigate('/mm/admin');
+        } catch (err) {
+            // error with login authentication
+            setError('Invalid email or password.');
+        }
+    };
+
+    // render a login UI
+    return (
+        <p></p>
+    );
 };
 
 export default Login;

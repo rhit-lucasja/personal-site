547720231a80418c67a972aa3f2ec2b00325d420
rhit-lucasja <lucasja@rose-hulman.edu>
2026-03-03 19:18:39 -0500

    added alert upon successful pick submission

--- src/components/mm/PicksForm.jsx
+++ src/components/mm/PicksForm.jsx
@@ -59,6 +59,7 @@ const PicksForm = () => {
         setSaving(true); // flag that we're in process of writing to Firebase
         try {
             await updateDoc(doc(db, 'participants', selectedParticipant), {picks});
+            alert(`Picks submitted for ${selectedParticipant}!`);
         } catch (err) {
             console.error(err);
             alert('Error saving picks.');

aa9baa1de28820eedcb16f70133ac019772109a8
rhit-lucasja <lucasja@rose-hulman.edu>
2026-06-03 16:35:45 -0400

    reversed order of project cards so most recent on top

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,13 +1,53 @@
 export const projects = [
     {
-        id: 'project-one',
-        title: 'Rose Bonfir',
+        id: 'project-seven',
+        title: 'BinaryHeart Inventory System',
+        summary: 'possibly my favorite',
+        description: 'so many details',
+        tech: ['React', 'TypeScript', 'Java', 'Javalin', 'PostgreSQL'],
+        github: 'GitHub',
+        live: 'host site',
+        images: []
+    },
+    {
+        id: 'project-six',
+        title: 'Qiskit Shor DLP',
         summary: 'a basic summary',
-        description: 'Lots of info here to be filled out later',
-        tech: ['Java', 'Swing'],
-        github: 'GitHub link',
+        description: 'more info',
+        tech: ['Python', 'Qiskit'],
+        github: 'Github',
+        live: 'host',
+        images: []
+    },
+    {
+        id: 'project-five',
+        title: 'March Madness Competition',
+        summary: 'summary',
+        description: 'description',
+        tech: ['React', 'JavaScript', 'Tailwind', 'Firebase'],
+        github: 'Github link',
+        live: 'host',
+        images: []
+    },
+    {
+        id: 'project-four',
+        title: 'Verilog Processor Design',
+        summary: 'summarizing here',
+        description: 'more to say',
+        tech: ['Verilog', 'RISC-V Assembly'],
+        github: 'Github',
         live: 'host link',
-        images: ['src/assets/projects/bonfir_home.png']
+        images: []
+    },
+    {
+        id: 'project-three',
+        title: 'xv6 Multithreading',
+        summary: 'a basic summary',
+        description: 'more info for later',
+        tech: ['C', 'RISC-V Assembly', 'xv6'],
+        github: 'Github',
+        live: 'host link',
+        images: []
     },
     {
         id: 'project-two',
@@ -20,13 +60,13 @@ export const projects = [
         images: ['src/assets/projects/xor_est.png']
     },
     {
-        id: 'project-three',
-        title: 'xv6 Multithreading',
+        id: 'project-one',
+        title: 'Rose Bonfir',
         summary: 'a basic summary',
-        description: 'more infor for later',
-        tech: ['C', 'RISC-V Assembly', 'xv6'],
-        github: 'Github',
+        description: 'Lots of info here to be filled out later',
+        tech: ['Java', 'Swing'],
+        github: 'GitHub link',
         live: 'host link',
-        images: []
-    }
+        images: ['src/assets/projects/bonfir_home.png']
+    },
 ];

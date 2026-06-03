9d6035c01ff0e63417ef11b86dc03fc42b221091
rhit-lucasja <lucasja@rose-hulman.edu>
2026-06-03 19:40:53 -0400

    thumbnail images

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,10 +1,16 @@
-import bonfirHome from '../assets/projects/bonfir_home.png';
+import bonfirCover from '../assets/projects/bonfir_cover.png';
+import xorCover from '../assets/projects/xor_cover.jpg';
 import xorEst from '../assets/projects/xor_est.png';
 import xorAcc from '../assets/projects/xor_acc.png';
 import xorCost from '../assets/projects/xor_cost.png';
+import osCover from '../assets/projects/os_cover.jpg';
 import osUnbuffered from '../assets/projects/os_unbuffered.png';
 import osTests from '../assets/projects/os_tests.png';
 import osTestsRun from '../assets/projects/os_tests_run.png';
+import cpuCover from '../assets/projects/cpu_cover.jpg';
+import shorCover from '../assets/projects/shor_cover.jpg';
+import mmCover from '../assets/projects/mm_cover.jpg';
+import bhCover from '../assets/projects/bh_cover.png';
 
 export const projects = [
     {
@@ -15,29 +21,29 @@ export const projects = [
         tech: ['React', 'TypeScript', 'Java', 'Javalin', 'PostgreSQL'],
         github: null,
         live: 'https://inventory.binaryheart.org',
-        thumbnail: null,
+        thumbnail: bhCover,
         images: []
     },
     {
         id: 'project-six',
-        title: 'Qiskit Shor DLP',
-        summary: 'a basic summary',
-        description: 'more info',
-        tech: ['Python', 'Qiskit'],
-        github: null,
-        live: null,
-        thumbnail: null,
-        images: []
-    },
-    {
-        id: 'project-five',
         title: 'March Madness Competition',
         summary: 'summary',
         description: 'description',
         tech: ['React', 'JavaScript', 'Tailwind', 'Firebase'],
         github: null,
         live: 'https://jack-lucas-site.vercel.app/mm/bracket',
-        thumbnail: null,
+        thumbnail: mmCover,
+        images: []
+    },
+    {
+        id: 'project-five',
+        title: 'Qiskit Shor DLP',
+        summary: 'a basic summary',
+        description: 'more info',
+        tech: ['Python', 'Qiskit'],
+        github: null,
+        live: null,
+        thumbnail: shorCover,
         images: []
     },
     {
@@ -48,7 +54,7 @@ export const projects = [
         tech: ['Verilog', 'RISC-V Assembly'],
         github: null,
         live: null,
-        thumbnail: null,
+        thumbnail: cpuCover,
         images: []
     },
     {
@@ -59,7 +65,7 @@ export const projects = [
         tech: ['C', 'RISC-V Assembly', 'xv6'],
         github: null,
         live: null,
-        thumbnail: null,
+        thumbnail: osCover,
         images: [osUnbuffered, osTests, osTestsRun]
     },
     {
@@ -70,7 +76,7 @@ export const projects = [
         tech: ['Python', 'NumPy', 'Matplotlib'],
         github: null,
         live: null,
-        thumbnail: null,
+        thumbnail: xorCover,
         images: [xorEst, xorCost, xorAcc]
     },
     {
@@ -81,7 +87,7 @@ export const projects = [
         tech: ['Java', 'Swing'],
         github: null,
         live: null,
-        thumbnail: bonfirHome,
+        thumbnail: bonfirCover,
         images: []
     },
 ];

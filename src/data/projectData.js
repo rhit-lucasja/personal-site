25c702646c9b37f86f2fc8ac4f628333a32e325d
rhit-lucasja <lucasja@rose-hulman.edu>
2026-06-04 00:13:55 -0400

    added info to the xor and os projects

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -76,10 +76,10 @@ export const projects = [
     {
         id: 'project-three',
         title: 'xv6 Multithreading',
-        summary: 'a basic summary',
-        description: 'further description',
-        feature: null,
-        design: null,
+        summary: 'A custom threading library for the xv6 operating system',
+        description: 'Introducing rhthr, the palindromic and tongue-twisting threading library designed and developed by myself, Tal Belkind, and Oskar Steiger. With this library, any user program can create multiple threads, execute them in parallel (or as close to parallel as xv6 allows), and join threads upon completion.',
+        feature: 'Threads share the same physical memory. Threads can also pass pointers between each other, accessing and updating data at specified addresses. Finally, any changes to one thread\'s pagetable propagate to all threads of the same process.',
+        design: 'Our team took a unique approach to testing. Instead of having threads print to stdout and manually searching for unexpected behavior, we created an automated testing suite by redirecting stdout during the creation and execution of the threads to be tested. First, we had to re-write xv6\'s printf() function, which initially wrote to stdout one character at a time (leading to the muddled output shown in the first image above). After buffering printf(), we could provide reliable expected outputs for our tests to use as comparison.',
         results: null,
         tech: ['C', 'RISC-V Assembly', 'xv6'],
         github: null,
@@ -90,16 +90,16 @@ export const projects = [
     {
         id: 'project-two',
         title: 'XOR Neural Network',
-        summary: 'a basic summary',
-        description: 'details',
+        summary: 'A fully connected neural network operating on the XOR dataset',
+        description: 'During the summer of 2024, I studied the basics of machine learning by creating a neural network operating on the MNIST dataset. To reinforce my skills, I adapted the MNIST model to the XOR dataset. For each random input (x,y), the neural network should output 0 whenever x and y have the same sign and 1 when x and y have different signs.',
         feature: null,
         design: null,
-        results: null,
+        results: 'The learning cycle featured in the images above lasted 25 epochs, achieving an ultimate accuracy of 92%. Though the structure and hyperparameters of the network could use more optimization, I did implement techniques, such as a variable learning rate and L2 regularization, to improve this basic network. I also created a scatterplot of the 10,000 points of evaluation data, visualized on a [-1,1] X [-1,1] coordinate grid. Each point is colored blue (1) or red (0), based on what the neural network in its final state outputs for that coordinate pair as input. The network separates the quadrants with fair accuracy, failing to classify only the points lying close to the axes.',
         tech: ['Python', 'NumPy', 'Matplotlib'],
         github: null,
         live: null,
         thumbnail: xorCover,
-        images: [xorEst, xorCost, xorAcc]
+        images: [xorCost, xorAcc, xorEst]
     },
     {
         id: 'project-one',
